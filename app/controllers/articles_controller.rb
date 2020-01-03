032be57efe87b4ebb261ccbf51848ce6b125071f
freedom <mayankrathore26@gmail.com>
2020-01-03 17:17:04 +0530

    Added Partials and Destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,17 @@
 class ArticlesController < ApplicationController
+    def index
+     @articles=Article.all
+    end
+    
     def new
         @article=Article.new
+    end     
+
+    def edit
+        @article=Article.find(params[:id])
+
     end
+
     def create
         @article=Article.new(article_params)
          if @article.save
@@ -12,8 +22,25 @@ class ArticlesController < ApplicationController
          end
     end
 
+    def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+        flash[:notice]="Article was successfully updated"
+        redirect_to article_path(@article)
+    else
+        render 'new'
+    end
+    end
+
     def show
         @article=Article.find(params[:id])
+    end
+
+    def destroy
+        @article =Article.find(params[:id])
+        @article.destroy
+        flash[:notice]="Article was Successfully destroyed"
+        redirect_to articles_path
     end
     private
         def article_params
